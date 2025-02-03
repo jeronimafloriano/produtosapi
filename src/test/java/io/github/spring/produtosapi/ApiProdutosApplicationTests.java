a1602f2a3fca08115dbdbbe123b56d7f681592e0
jeronima <jeronima.floriano@elo.com.br>
2025-02-03 08:00:42 -0300

    Adicionando arquivos iniciais

---
+++ src/test/java/io/github/spring/produtosapi/ApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spring.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
